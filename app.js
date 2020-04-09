97a0a830b5c4a079d6bb29addaaf1a2366e2d87f
Ridwan Hady <ridwanhady31@gmail.com>
2020-04-09 23:33:54 +0700

    Add dotenv to process .env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
