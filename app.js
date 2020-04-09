a9b4ba6e0e990a3773aa6c101a14ccaf8b4d6d5e
Ridwan Hady <ridwanhady31@gmail.com>
2020-04-09 23:00:29 +0700

    Add initial app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
