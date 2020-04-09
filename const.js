eedc151ea140da5836e53d89f1ef8c1cebb88bec
Ridwan Hady <ridwanhady31@gmail.com>
2020-04-10 01:42:54 +0700

    Add conversation constant

---
+++ const.js
@@ -0,0 +1,16 @@
+module.exports = {
+  UNREGISTERED: `Hi! I am Birthday Bot.\n
+  I think this is our first encounter. May I know your first name?`,
+  INVALID_NAME: `Sorry, could you please repeat, what is your first name?`,
+  NAME_ADDED: `Hi %s! Nice to meet you.\n
+  I want to know more about you, could you tell me birthday?\n
+  Please tell me in DD-MM-YYYY format.\nAnd uh, mine is 10-04-2020`,
+  INVALID_DATE: `Please input your birthday in DD-MM-YYYY format, thanks :)`,
+  DATE_ADDED: `Thank you.\n
+  Do you want to know how many days till your next birthday?`,
+  ASKING_NEXT_BIRTHDAY: `Hi %s!\n
+  Do you want to know how many days till your next birthday?`,
+  GOODBYE: `Goodbye~~`,
+  NEXT_BIRTHDAY: `There are %d days left until your next one!\n
+  I believe it will be a good birthday for you`,
+};
